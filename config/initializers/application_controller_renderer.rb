bb7009c62555f99a4ef198031e0eba828e56cbb7
daisyo0090 <daisho0090@gmail.com>
2021-08-04 12:14:27 +0000

    [Finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'fbdc27a90b600a86e0eb407d95480a870b1a3d0566bdc9b7e6d8ef666806e3ad22d19cb4bc31cdda1daaec1ce9b4458323a8bc8c5aa4ffa9813885b3265d13b0'
